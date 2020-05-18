425c4ab54770a10a981b4a21a2c316cccd40e0d1
FuadEsgerov <56359250+FuadEsgerov@users.noreply.github.com>
2020-05-18 22:09:53 +0400

    Controllers,Views added

---
+++ Jotex/Jotex/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jotex.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detailed()
+        {
+            return View();
+        }
+    }
+}
---
+++ Jotex/Jotex/Controllers/CaseStudyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jotex.Controllers
+{
+    public class CaseStudyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CaseSingle()
+        {
+            return View();
+        }
+    }
+}
